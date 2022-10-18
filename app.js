374cade4a8b1e8a2f1257c80e5aab94f755ea134
IERoldan <ivanroldan1989@gmail.com>
2022-10-18 11:11:31 -0300

    deleted all comments

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let app = express();
 let user_routes = require('./routes/user.routes');
 let menu_routes = require('./routes/menu.routes');
 let category_routes = require('./routes/category.routes')
-// let request_routes = require('./routes/request.routes')
 let cors = require('cors')
 
 app.use(cors());
